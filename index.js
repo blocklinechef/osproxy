1512890eb30ba5de3a3f0b918c08974f8277a77f
blocklinechef <cethereum@protonmail.com>
2022-07-12 18:09:33 +0800

    Added mongodb

--- index.js
+++ index.js
@@ -1,16 +1,48 @@
-import { WebSocket } from 'ws';
-import { OpenSeaStreamClient } from "@opensea/stream-js";
-import axios from 'axios';
-import ethers from 'ethers';
-import 'dotenv/config';
+const { MongoClient } = require('mongodb');
+const { WebSocket } = require('ws');
+const { OpenSeaStreamClient } = require('@opensea/stream-js');
+const axios = require('axios');
+require('dotenv/config');
 
-const client = new OpenSeaStreamClient({
+const osclient = new OpenSeaStreamClient({
     token: process.env.OPENSEA_TOKEN,
     connectOptions: {
         transport: WebSocket
     }
 });
 
-client.onItemTransferred('*', (event) => {
+const itemMetaDataUpdated = async (event) => {
     console.log(event);
-});
+};
+
+const itemListed = async (event) => {
+    console.log(event);
+};
+
+const itemSold = async (event) => {
+    console.log(event);
+};
+
+const itemTransferred = async (event) => {
+    console.log(event);
+};
+
+const itemReceivedBid = async (event) => {
+    console.log(event);
+};
+
+const itemReceivedOffer = async (event) => {
+    console.log(event);
+};
+
+const itemCancelled = async (event) => {
+    console.log(event);
+};
+
+osclient.onItemMetadataUpdated('*', itemMetaDataUpdated);
+osclient.onItemListed('*', itemListed);
+osclient.onItemSold('*', itemSold);
+osclient.onItemTransferred('*', itemTransferred);
+osclient.onItemReceivedBid('*', itemReceivedBid);
+osclient.onItemReceivedOffer('*', itemReceivedOffer);
+osclient.onItemCancelled('*', itemCancelled);
